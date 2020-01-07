5b531de5ca21b6250ab03107661ef0fcc22b814d
Mbarley <mbarley66@gmail.com>
2020-01-07 14:08:34 -0700

    fileExporter updated

--- exportUtilities/fileExporter.js
+++ exportUtilities/fileExporter.js
@@ -1,120 +1,94 @@
 
 //PUT RABBIT CONFIG IN HERE
-var neo4j = require('neo4j-driver').v1;
+var neo4j = require('neo4j-driver');
 var Query = require('decypher').Query;
 const fs = require('fs');
 var Jimp = require('jimp');
-const fs = require('fs');
 const request = require('request');
 const rp = require('request-promise');
 const URL = require('url').URL;
 
-  // Librarin Config
-  var librarianConfig= {
-    "enable": true,
-    "host": "http://localhost:10004",
-    "username": "botuser",
-    "password": "CodaGlenBaronyMonk"
-  }
-  //NEO4J CONFIG
-  var neoConf={
-    "url": "bolt://localhost:10002",
-    "user": "neo4j",
-    "pass": "CodaGlenBaronyMonk",
-  }
-  var driver = neo4j.driver(neoConf.url, neo4j.auth.basic(neoConf.user, neoConf.pass));
-  var session = driver.session();
+// Librarin Config
+var librarianConfig= {
+  "host": "http://localhost:10004",
+  "username": "botuser",
+  "password": "CodaGlenBaronyMonk"
+}
+//NEO4J CONFIG
+var neoConf={
+  "url": "bolt://localhost:10002",
+  "user": "neo4j",
+  "pass": "CodaGlenBaronyMonk",
+}
+var driver = neo4j.driver(neoConf.url, neo4j.auth.basic(neoConf.user, neoConf.pass));
+var session = driver.session();
 
- //NEO4J QUERY
- var queryBuilder = function() {
-    var query = new Query();
-    query.match(" (p:Page)-[]-(t:Table) with p,t ")
-    // query.match("(p:Page)-[:HAS_FACET]-(pr:Processing {Name:'Table Extraction Failed'}) where p.HasTable=true  with collect(distinct p) as chemistryPages unwind chemistryPages as p")
-    //query.match("(p:Page)-[:HAS_CHEMISTRY]-(i:Instance) where p.HasTable=true and not exists(p.textractSuccess) with collect(distinct p) as chemistryPages unwind chemistryPages as p")
-    query.return("split(t.Filename,'.')[0] + '-page-' + t.PageNumber +'.csv' as filename, t.CSV as csv")
-    return query;
-  }
+//NEO4J QUERY
+var queryBuilder = function() {
+  var query = new Query();
+  query.match(" (f:File)")
+  query.return("f.LibraryKey as Library, f.LibraryPath as Path, f.Name as Name")
+  return query;
+}
 
 
-
-  var query = queryBuilder();
-  return session.run(query.compile(), query.params()).then(async function(result){
-    driver.close();
-    console.log("pulled " + result.records.length + " records from graph")
-    for(var i = 0; i < result.records.length; i++){
-      //get data
-      var filename =result.records[i].get("filename")
-      var data=result.records[i].get("csv")
-
-      // get image for table pulled
-      var filePath= "textract-" + msg.Uuid + ".png"
-      return getFile(msg.Library, msg.Path, filePath).then((filePath)=>{
-        return this.sizeImage(filePath)
-
-      })
-      console.log(filename + ' ' +data)
-     
-  }
-
-  // Get the file from the Librarian. Returns a promise with a path to the file.
-  // If the file already exists, don't re-download it.
-  this.getFile = function(libkey, libpath, localpath) {
-    if(fs.existsSync(localpath)){
-      return Promise.resolve(localpath);
-    }else
-      return download(libkey, libpath, localpath)
-  }
-
-  this.sizeImage = function(filePath){
-    return new Promise((resolve,reject) => {
-      if(fs.statSync(filePath).size > 1024000){
-        Jimp.read(filePath).then(file => {
-          file.quality(60).greyscale().writeAsync(filePath.slice(0,-4)+".jpg")
-          return resolve(filePath.slice(0,-4)+".jpg")
-        })
-        .catch(err => {
-          return reject(err);
-        });
-      }else{
-        return resolve(filePath);
-      }
-    })
-  }
-    this.download = (key,path,filePath) => {
-    var url =  new URL('/file', librarianConfig.host);
-    
-    url.searchParams.set("library",key);
-    url.searchParams.set("path",path);
-    var opts = {
-      url: url.toString(),
-      qs: {
-        library: key,
-        path: path
-      },
-      auth: {
-        user: librarianConfig.username,
-        pass: librarianConfig.password
-      }
+var download = (key,path,filePath) => {
+  var url =  new URL('/file', librarianConfig.host);
+  
+  url.searchParams.set("library",key);
+  url.searchParams.set("path",path);
+  var opts = {
+    url: url.toString(),
+    qs: {
+      library: key,
+      path: path
+    },
+    auth: {
+      user: librarianConfig.username,
+      pass: librarianConfig.password
     }
-
-    return new Promise((resolve,reject) => {
-      request(opts)
-        .on('error', (err) => {
-          return reject(err);
-        })
-        .pipe(fs.createWriteStream(filePath))
-        .on('error', (err) => {
-          return reject(err);
-        })
-        .on('finish', () => {
-          return resolve(filePath);
-        })
-    });
   }
-
-  this.initialize = function(){
+  console.log(opts)
+  return new Promise((resolve,reject) => {
+    request(opts)
+      .on('error', (err) => {
+        return reject(err);
+      })
+      .pipe(fs.createWriteStream(filePath))
+      .on('error', (err) => {
+        return reject(err);
+      })
+      .on('finish', () => {
+        return resolve(filePath);
+      })
+  });
+}
+// Get the file from the Librarian. Returns a promise with a path to the file.
+// If the file already exists, don't re-download it.
+var getFile = function(libkey, libpath, localpath) {
+  // if(fs.existsSync(localpath)){
+  //   return Promise.resolve(localpath);
+  // }else
+    return download(libkey, libpath, localpath)
+}
+//start
+var query = queryBuilder();
+return session.run(query.compile(), query.params())
+.then(async function(result){
+  driver.close();
+  console.log("pulled " + result.records.length + " records from graph")
+  for(var i = 0; i < result.records.length; i++){
+    //get data
+    var lib =result.records[i].get("Library")
+    var path =result.records[i].get("Path")
+    var name = result.records[i].get("Name")
+    // get image for table pulled
+    getFile(lib, path, "output/"+name).then((filePath)=>{
+      console.log(filePath)
+    })
+    
   }
-
 })
+.catch( function(err){console.log("Error: %s",err)})
 
 
